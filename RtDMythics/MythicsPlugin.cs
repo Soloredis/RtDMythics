967c1f58a29c9d6124084bca97c684798d291cc8
proteus <117112022+Soloredis@users.noreply.github.com>
2026-09-12 15:41:24 -0500

    Shield Bubble SFX Voume reduction

--- RtDMythics/MythicsPlugin.cs
+++ RtDMythics/MythicsPlugin.cs
@@ -21,7 +21,7 @@ namespace RtDMythics
 
         public const string PluginName = "RtDMythics";
 
-        public const string PluginVersion = "1.3.14";
+        public const string PluginVersion = "1.3.15";
 
         private AssetBundle MyAssets;
 
